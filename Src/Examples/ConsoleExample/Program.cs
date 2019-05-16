f981a9d3b172c9b1773d4b0502e2041cdbcc4097
Ous <regous@163.com>
2019-05-16 23:52:13 +0800

    增加命令行测试项目

---
+++ Src/Examples/ConsoleExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Lifetime.Clear;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (null == System.Windows.Application.Current)
+            {
+                new System.Windows.Application();
+            }
+            ViewModel aViewModel = new ViewModel();
+            aViewModel._notifier.ShowSuccess("1");
+        }
+
+
+    }
+
+    class ViewModel
+    {
+        public readonly Notifier _notifier;
+        public ViewModel()
+        {
+            _notifier = new Notifier(cfg =>
+            {
+                cfg.PositionProvider = new WindowPositionProvider(
+                    parentWindow: Application.Current.MainWindow,
+                    corner: Corner.BottomRight,
+                    offsetX: 25,
+                    offsetY: 100);
+
+                cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                    notificationLifetime: TimeSpan.FromSeconds(6),
+                    maximumNotificationCount: MaximumNotificationCount.FromCount(6));
+
+                cfg.Dispatcher = Application.Current.Dispatcher;
+
+                cfg.DisplayOptions.TopMost = false;
+                cfg.DisplayOptions.Width = 250;
+            });
+
+            _notifier.ClearMessages(new ClearAll());
+        }
+    }
+
+}
